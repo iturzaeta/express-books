6ba79ae3a86f0d9fababd5e8c394911c4830c08a
Carol Alonso <c.alonsolp@gmail.com>
2019-11-05 20:23:08 +0100

    task: Book model, list and detail

--- config/routes.js
+++ config/routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/base.controller');
 
 router.get('/', controller.base);
+router.get('/books', controller.listBooks);
+router.get('/books/:id', controller.bookDetail);
 
 module.exports = router;
--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -1,8 +1,37 @@
 const createError = require('http-errors');
 const mongoose = require('mongoose');
+const Book = require('../models/book.model');
 
 module.exports.base = (req, res, next) => {
     res.render('index', {
         title: 'Welcome to your CRUD project'
     });
+};
+
+module.exports.listBooks = (req, res, next) => {
+    Book.find()
+        .then(
+            books => {
+                res.render('books/list', { books })
+            }
+        ).catch(
+            error => next(error)
+        );
+};
+
+module.exports.bookDetail = (req, res, next) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        next(createError(404));
+    } else {
+        Book.findById(id)
+            .then(
+                book => {
+                    res.render('books/detail', { book })
+                }
+            ).catch(
+                error => next(error)
+            );
+    }
 };
