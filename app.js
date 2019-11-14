366418cf639967d09a9ba8fc0e50a994b8d04d8b
Carol Alonso <c.alonsolp@gmail.com>
2019-11-14 15:32:27 +0100

    fix: bad indentation

--- app.js
+++ app.js
@@ -34,18 +34,18 @@ app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 /** 
@@ -53,7 +53,7 @@ app.use(function (err, req, res, next) {
  */
 const port = normalizePort(process.env.PORT || '3000');
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
 
 // Helper functions
@@ -62,14 +62,14 @@ app.listen(port, () => {
  * Normalize a port into a number, string, or false.
  */
 function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-    return false;
+	var port = parseInt(val, 10);
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+	return false;
 }
--- models/celebrity.model.js
+++ models/celebrity.model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const celebritySchema = new Schema(
-    {
-        name: String,
-        catchPhrase: String,
-        movies: [{ type: Schema.Types.ObjectId, ref: 'Movie' }]
-    }
+	{
+		name: String,
+		catchPhrase: String,
+		movies: [{ type: Schema.Types.ObjectId, ref: 'Movie' }]
+	}
 );
 
 const Celebrity = mongoose.model('Celebrity', celebritySchema);
